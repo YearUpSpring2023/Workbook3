0eb3219380202eb70b01d5485abff46bd39ea5d4
mattchristenson <matt@rcc.team>
2023-05-02 09:20:58 -0500

    first exercise together

---
+++ Functions/simple_functions.js
@@ -0,0 +1,42 @@
+"use strict";
+
+function favoriteThings(){
+    let name = ("Zion");
+    let favMovie = ("SeventeenAgain");
+    let favMusician = ("JanetJackson");
+    
+    console.log("My name is " + name + ". " 
+    + "My favorite movie right now is " + favMovie + ". " 
+    + "My favorite musician is " + favMusician + ".");
+}
+
+
+function whyImHere(){
+
+    let reason1 = ("I joined this Learning");
+    let reason2 = (" to Code program to become financially free and to help others.");
+
+    console.log("Here is my sentence: " + reason1 + reason2);
+
+}
+
+function favoritePlace() {
+    let unitedStates = ("miami because I like the weather");
+    let spain = ("thats where my favorite soccer club is from");
+    
+    console.log("My favorite place in USA is " + unitedStates + "as for spain I like barcelona because" + spain);
+}
+
+
+console.log("Zion favorite things");
+favoriteThings();
+favoriteThings();
+favoriteThings();
+
+
+console.log("Deshonda Why Im Here");
+whyImHere();
+
+console.log("Kevin favorite places");
+favoritePlace();
+favoritePlace();
